495b950ffea9afdf64b9f704ee2dc9bd209b705f
ShinjiKamei <mfndn101@yahoo.co.jp>
2015-05-21 11:49:01 +0900

    Finish practices of chapt6.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
   
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  #↑1字以上の英数字 "-"or"." @ 1字以上の英数字or"-" 0字以上の("."or1字以上の英字) "." 1字以上の英字
   
-  before_save { self.email = email.downcase }
+  #before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   
   validates(:name,  presence: true, length: { maximum: 50 })
   validates(:email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
