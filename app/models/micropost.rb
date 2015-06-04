f6f03598cd9f3171d53619e4a41bfcd236610b60
ShinjiKamei <mfndn101@yahoo.co.jp>
2015-06-04 17:07:36 +0900

    debug

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -15,7 +15,7 @@ class Micropost < ActiveRecord::Base
     #followed_user_ids = user.followed_user_ids
     followed_user_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
     #where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
-    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", user_id: user)
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", user_id: user.id)
   end
   
 end
